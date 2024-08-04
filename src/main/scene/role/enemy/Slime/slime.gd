420975a7142aa05a9752ba5bc54962d032a7e39d
EricSanchez <niexiaohangeric@163.com>
2024-08-04 17:22:59 +0800

    最新同步

--- src/main/scene/role/enemy/Slime/slime.gd
+++ src/main/scene/role/enemy/Slime/slime.gd
@@ -16,40 +16,6 @@ func _ready() -> void:
 				"SLOW_RES": 0.1,
 				"FRZ_RES": 0.0
 			}
-		"B":
-			base_attributes = {
-				"MAX_HP": 100.0,
-				"HP": 100.0,
-				"HP_REGEN": 0.5,
-				"MOV_SPD": 50.0,
-				"ACCEL": 2000.0,
-				"DMG": 1.0,
-				"KNB_RES": 0.1,
-				"SLOW_RES": 0.1,
-				"FRZ_RES": 0.0
-			}
-		"C":
-			base_attributes = {
-				"MAX_HP": 100.0,
-				"HP": 100.0,
-				"HP_REGEN": 0.5,
-				"MOV_SPD": 50.0,
-				"ACCEL": 2000.0,
-				"DMG": 1.0,
-				"KNB_RES": 0.1,
-				"SLOW_RES": 0.1,
-				"FRZ_RES": 0.0
-			}
-		"D":
-			base_attributes = {
-				"MAX_HP": 100.0,
-				"HP": 100.0,
-				"HP_REGEN": 0.5,
-				"MOV_SPD": 50.0,
-				"ACCEL": 2000.0,
-				"DMG": 1.0,
-				"KNB_RES": 0.1,
-				"SLOW_RES": 0.1,
-				"FRZ_RES": 0.0
-			}
+		_:
+			printerr("No match enemy_type!")
 
